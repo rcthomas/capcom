d900475400c8eeece1e732c6901fbbd068c40712
R. C. Thomas <rcthomas@lbl.gov>
2015-08-28 14:30:29 -0700

    Handle readonly/readwrite properly.

--- capcom/dbi/cosmo_image.py
+++ capcom/dbi/cosmo_image.py
@@ -8,22 +8,24 @@ class CosmoImageMetadata ( MongoDBI ) :
 
     @classmethod
     def read_only( cls ) :
-        return CosmoImageMetadata()
+        return CosmoImageMetadata( "readonly" )
 
     @classmethod
     def read_write( cls ) :
         username = os.environ[ "MONGODB_CIM_RW_USER" ]
         password = os.environ[ "MONGODB_CIM_RW_PASS" ]
-        return CosmoImageMetadata( username, password )
+        return CosmoImageMetadata( "readwrite", username, password )
 
-    def __init__( self, username = None, password = None, hostname = None ) :
-        username = username or os.environ[ "MONGODB_CIM_RO_USER" ]
-        password = password or os.environ[ "MONGODB_CIM_RO_PASS" ]
-        hostname = hostname or os.environ[ "MONGODB_HOST"        ]
-        dbname   = os.environ[ "MONGODB_CIM" ]
+    def __init__( self, mode, username = None, password = None, hostname = None ) :
+        self.mode = mode
+        username  = username or os.environ[ "MONGODB_CIM_RO_USER" ]
+        password  = password or os.environ[ "MONGODB_CIM_RO_PASS" ]
+        hostname  = hostname or os.environ[ "MONGODB_HOST"        ]
+        dbname    = os.environ[ "MONGODB_CIM" ]
         super( CosmoImageMetadata, self ).__init__( username, password, hostname, dbname )
 
     @lazy
     def decam_public( self ) :
-        self.database.decam_public.ensure_index( [ ( "relpath", 1 ), ( "position", 1 ) ], unique = True )
+        if self.mode == "readwrite" :
+            self.database.decam_public.ensure_index( [ ( "relpath", 1 ), ( "position", 1 ) ], unique = True )
         return self.database.decam_public
